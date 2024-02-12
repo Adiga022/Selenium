05d768b204183f5006a54288481ea79ed4a8e890
Ислам <153752914+Adiga022@users.noreply.github.com>
2024-02-12 11:11:27 +0300

    Add files via upload

---
+++ PythonProjects/tests/front/test_shop.py
@@ -0,0 +1,33 @@
+# импортируем модули и отдельные классы
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+# каждый тест должен начинаться с test_
+def test_product_view_sku():
+    """
+    Test case WERT-1
+    """
+		# Описываем опции запуска браузера
+    chrome_options = Options()
+    chrome_options.add_argument("start-maximized") # открываем на полный экран
+    chrome_options.add_argument("--disable-infobars") # отключаем инфо сообщения
+    chrome_options.add_argument("--disable-extensions") # отключаем расширения
+    # chrome_options.add_argument("--headless") # спец. режим "без браузера"
+	
+		# устанавливаем webdriver в соответствии с версией используемого браузера
+    service = Service()
+    # запускаем браузер с указанными выше настройками
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+		# определяем адрес страницы для теста и переходим на неё
+    URL = "https://testqastudio.me/"
+    driver.get(url=URL)
+    element = driver.find_element(by=By.CSS_SELECTOR, value='#main [class*="tab-best_sellers"]')
+    element.click()
+    element = driver.find_element(by=By.CSS_SELECTOR, value= '[class*="post-11341"]')
+    element.click()
+    sku = driver.find_element(by=By.CLASS_NAME, value='sku')
+    assert sku.text == 'C0MSSDSUM7', 'Неверный артикул'
